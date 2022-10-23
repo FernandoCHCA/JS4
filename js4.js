e2c34ceb5d299fa3766fea184e9f6f267dce2663
Jose Fernando Chavez Castillo <josefernandochca@gmail.com>
2022-10-23 14:31:20 -0500

    Actualizacion de JS4

---
+++ js4.js
@@ -0,0 +1,8 @@
+horaInicial = Number(prompt("Ingrese la hora inicial (0-23):"));
+
+frecuencia = Number(prompt("Ingrese la frecuencia (1-12):"));
+
+document.write("Hora incial: ", horaInicial," (tipo de dato: ",typeof(horaInicial),")");
+document.write("<br/>Frecuencia: ", frecuencia," (tipo de dato: ",typeof(frecuencia),")");
+
+// Verifica si se va a saltar a otro día
